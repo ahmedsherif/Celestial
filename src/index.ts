1628a4ea1377226e625a3a24f266c468096152a9
hirusi <inbox@rusingh.com>
2020-08-19 11:55:51 +0530

    Revert "fix: duplicate imports"
    This reverts commit 9ae7d907515fe1edf30aeb9b530d61dc5c40194d.

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,10 @@ import { authRouter } from "routes/authentication";
 import { logoutRouter } from "routes/logout";
 import { publishRouter } from "routes/publish";
 import { userRouter } from "routes/user";
+import { authRouter } from "./routes/authentication";
+import { logoutRouter } from "./routes/logout";
+import { publishRouter } from "./routes/publish";
+import { userRouter } from "./routes/user";
 
 // Our interface, enums, libs, etc.
 import { AppUserState } from "enumerator/AppUserState";
