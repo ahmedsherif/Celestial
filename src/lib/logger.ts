a7846d87cca85c8e665527f053906c0538310b46
hirusi <inbox@rusingh.com>
2020-08-20 19:58:05 +0530

    build: console logging only on dev env
    additionally creates an all.log for debugging purposes, info.log for lighter debugging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,19 +2,21 @@ import { createLogger, format, transports } from "winston";
 const { combine, prettyPrint, timestamp } = format;
 
 const logger = createLogger({
-	level: process.env.NODE_ENV === "production" ? "info" : "silly",
+	level: "silly",
 	format: format.json(),
 	defaultMeta: { service: "Celestial" },
 	transports: [
 		// - Write all logs with level `error` and below to `error.log`
 		new transports.File({ filename: "error.log", level: "error" }),
-		// - Write all logs with level `info` and below to `combined.log`
-		// new transports.File({ filename: "combined.log" }),
+		// - Write all logs with level `info` and below to `info.log`
+		new transports.File({ filename: "info.log", level: "info" }),
+		// - Write all logs with level `silly` and below to `all.log`
+		new transports.File({ filename: "all.log", level: "silly" }),
 	],
 });
 
-// If we're not in production then log to the `console` as well
-if (process.env.NODE_ENV !== "production") {
+// If we're in development then log to the `console` as well
+if (process.env.NODE_ENV === "development") {
 	logger.add(
 		new transports.Console({
 			format: combine(
