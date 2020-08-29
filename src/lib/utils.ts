80d4f1866d5231f1579552181223c2741f2d2d65
hirusi <inbox@rusingh.com>
2020-08-29 10:30:25 +0530

    fix: timeout on logged-in navigation for non-guests/users

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,17 @@
 import {
 	Request as ExpressRequest,
 	Response as ExpressResponse,
+	NextFunction as ExpressNextFunction,
 } from "express";
 import { AppUserState } from "../enumerator/AppUserState";
 
-const protectedRoutes = (req: ExpressRequest, res: ExpressResponse) => {
+const protectedRoutes = (
+	req: ExpressRequest,
+	res: ExpressResponse,
+	next: ExpressNextFunction
+) => {
 	if (req.session?.appState !== AppUserState.User) res.redirect("/login/");
+	next();
 };
 
 export { protectedRoutes };
